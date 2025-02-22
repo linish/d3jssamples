5ef498b8fbec70a0706aa37f295dd52a55b1dd18
Linish V S <linish@riefmedia.com>
2025-02-22 09:56:17 +0530

    D3JS Sample With Transformation - Feb 22, 2025

---
+++ D3JS/SimpleD3JS/app.js
@@ -0,0 +1,9 @@
+// const pBrowser = document.querySelector('p');
+const el = d3
+  .select("body")
+  .append("p")
+  .attr("class", "foo")
+  .text("Hello World");
+
+// console.log(pBrowser);
+console.log(el);
